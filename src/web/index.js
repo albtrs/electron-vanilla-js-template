2dbac6b31323e9b5e11db2a6aa951394bdcbf184
Your Name <you@example.com>
2023-11-30 14:11:29 +0900

    update

--- src/web/index.js
+++ src/web/index.js
@@ -1,19 +1,7 @@
 //import 'ress/dist/ress.min.css';
 import './index.css';
 
-function popup() {
-  //const text = document.getElementById("sample-text").value;
-  alert('????');
-}
-
-const btnTest = document.getElementById('sample-button');
-btnTest.addEventListener('click', async () => {
+const button = document.getElementById('sample-button');
+button.addEventListener('click', async () => {
     await window.apis.simpleDialog("OK");
 });
-
-//async function openDialog() {
-//  const result = await ipcRenderer.invoke('open-dialog', 'Hello, Electron!')
-//  console.log(result)
-//}
-//
-//openDialog()
